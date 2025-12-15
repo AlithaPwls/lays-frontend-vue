2a2538bed1f548411233e7c6119ec9dfc0954882
r1011638@student.thomasmore.be <r1011638@student.thomasmore.be>
2025-12-15 18:19:09 +0100

    feat: enhance designs view with loading state and responsive grid layout

--- src/views/DesignsView.vue
+++ src/views/DesignsView.vue
@@ -1,22 +1,41 @@
 <template>
-    <div>
-      <h1>Saved designs</h1>
+    <div class="designs-page">
+      <router-link to="/" class="back-link">
+        ← Back to configurator
+      </router-link>
   
-      <router-link to="/">← Back</router-link>
+      <h1>All designs</h1>
   
-      <div v-for="design in designs" :key="design._id">
-        {{ design.title }} – {{ design.font.name }}
+      <div v-if="loading" class="status">
+        Loading designs…
+      </div>
+  
+      <div v-else-if="designs.length === 0" class="status">
+        No designs yet
+      </div>
+  
+      <div v-else class="designs-grid">
+        <div
+          v-for="design in designs"
+          :key="design._id"
+          class="design-card"
+        >
+          <div
+            class="color-preview"
+            :style="{ backgroundColor: design.color }"
+          ></div>
+  
+          <h3
+            class="design-title"
+            :style="{ fontFamily: design.font }"
+          >
+            {{ design.title }}
+          </h3>
+  
+          <p class="design-font">
+            {{ design.font }}
+          </p>
+        </div>
       </div>
     </div>
   </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue'
-  
-  const designs = ref([])
-  
-  onMounted(async () => {
-    const res = await fetch('http://localhost:3000/designs')
-    designs.value = await res.json()
-  })
-  </script>
