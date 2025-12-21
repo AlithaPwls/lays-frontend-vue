fdafcfaaf9803355a9c0fa51059f66e90fd66d08
r1011638@student.thomasmore.be <r1011638@student.thomasmore.be>
2025-12-21 20:06:43 +0100

    style: simplify AdminView content and remove unnecessary elements

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,10 +1,5 @@
 <template>
-    <div style="padding: 40px; background: pink;">
-      <h1>ADMIN PANEL</h1>
-      <p>Als je dit ziet, werkt de admin route.</p>
+    <div>
+      ADMIN VIEW WERKT
     </div>
   </template>
-  
-  <script setup>
-  console.log('ADMIN VIEW LOADED')
-  </script>
